c161b7f193753c8366cace79ba4bee4d9b6c839e
jun68ykt <jun68ykt@teratail>
2019-08-12 07:38:09 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TL;DR
+
+This repo contains sample codes referred to my answer for the [question No.205429](https://teratail.com/questions/205429 ) of [teratail](https://teratail.com/), which is a Programming Q&A site of Japan.
+
+# How to install and run
+
+- yarn install
+- yarn start
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Layout, Header, Navigation,Drawer,Content} from 'react-mdl';
+
+
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{height: '300px', position: 'relative'}}>
+      <Layout fixedHeader>
+        <Header
+          className="header-color"
+          title={<span>The Title</span>}>
+          <Navigation>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
