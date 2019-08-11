c4221b11293e1ed599b64a48a42dbe5498a5bbe3
jun68ykt <jun68ykt@teratail>
2019-08-12 07:56:17 +0900

    material 関連のimportとlink追加

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-mdl/extra/material';
+import 'react-mdl/extra/material.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
